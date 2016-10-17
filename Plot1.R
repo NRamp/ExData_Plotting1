d1efce265c6be0201a0b2ad1bafbbd8252fdf08e
NR <nmum86@gmail.com>
2016-10-16 20:39:50 -0400

    added height and width and rechecked it in.

--- Plot1.R
+++ Plot1.R
@@ -18,7 +18,7 @@ names(pl1) <- c("Date","Time","Global_active_power",
 subpl1 <- subset(pl1,pl1$Date=="1/2/2007" | pl1$Date =="2/2/2007")
 
 ### opening a png device to write the plot
-png(filename="Plot1.png")
+png(filename="Plot1.png",width = 480, height = 480)
 
 #calling the basic plot function
 hist(as.numeric(as.character(subpl1$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
